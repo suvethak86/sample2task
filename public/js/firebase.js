8ab762cb95d891c5a3d882885ef58da19271044f
suvetha K <suvethak86@gmail.com>
2024-08-13 19:41:53 +0530

    added

---
+++ public/js/firebase.js
@@ -0,0 +1,13 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyCWwCXu60tN5jDnu7r_1Trhokc4qVbIjqw",
+    authDomain: "blogging-website-1e6a3.firebaseapp.com",
+    projectId: "blogging-website-1e6a3",
+    storageBucket: "blogging-website-1e6a3.appspot.com",
+    messagingSenderId: "493643151780",
+    appId: "1:493643151780:web:d1e6f8450733943c24e931"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const db = firebase.firestore();
